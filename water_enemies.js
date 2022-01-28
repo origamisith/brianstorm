c78afdb2b1dd65f45d9f027bf8b16cb19d5a3e4c
rinpham217 <rinpham1011@gmail.com>
2022-01-28 01:05:19 -0800

    update water_enemies.js

--- water_enemies.js
+++ water_enemies.js
@@ -3,26 +3,44 @@ class Squid {
         Object.assign(this, { game, x, y });
 
         this.game = game;
-
+        this.speed = 3;
         // spritesheet
         this.spritesheet = ASSET_MANAGER.getAsset();
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./Characters/squid/squid_sheet.png"), 0, 0, 800, 800, 5, 0.12, false, true);
-        
-
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/characters/squid/squid_sheet_new.png"), 0, 0, 200, 200, 5, 0.12, false, true);
+        this.updateBB();
     };
 
-
+    updateBB() {
+        this.lastBB = this.BB;
+        this.BB = new BoundingBox(this.x, this.y, 200, 200);
+    };
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 0, 0, 1);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 1);
     };
 
     update() {
     };
+};
+class Squid_ink {
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
 
-    setAttack(){
-        this.animator =  new Animator(ASSET_MANAGER.getAsset("./Characters/squid_ink/sqink.png"), 0, 0, 1000, 1000, 16, 0.1, false, true);
+        this.game = game;
+        this.speed = 3;
+        // spritesheet
+        this.spritesheet = ASSET_MANAGER.getAsset();
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/characters/squid_ink/squid_ink_sheet.png"), 0, 0, 300, 300, 16, 0.12, false, true);
+        this.updateBB();
     };
 
-  
+    updateBB() {
+        this.lastBB = this.BB;
+        this.BB = new BoundingBox(this.x, this.y, 300, 300);
+    };
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 1);
+    };
 
+    update() {
+    };
 };
