7700f70b9a719ec77993e5d6ba67d0cbf210253f
rinpham217 <rinpham1011@gmail.com>
2022-02-10 00:59:17 -0800

    updated

--- waterAnimals.js
+++ waterAnimals.js
@@ -6,15 +6,12 @@ class Seahorses {
         this.speed = 3;
         this.spritesheet = ASSET_MANAGER.getAsset();
         this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/characters/water_level/seahorse_fishes/seahorse_sheetnew.png"), 0, 0, 700, 700, 7, 0.12, false, true);
-        this.updateBB();
+      
     };
 
-    updateBB() {
-        this.lastBB = this.BB;
-        this.BB = new BoundingBox(this.x, this.y, 700, 700);
-    };
     draw(ctx) {
         this.animator.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 1);
+        
     };
 
     update() {
@@ -28,13 +25,9 @@ class Fishes {
         this.speed = 3;
         this.spritesheet = ASSET_MANAGER.getAsset();
         this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/characters/water_level/seahorse_fishes/smallfish_sheetnew.png"), 0, 0, 600, 600, 6, 0.12, false, true);
-        this.updateBB();
+        
     };
 
-    updateBB() {
-        this.lastBB = this.BB;
-        this.BB = new BoundingBox(this.x, this.y, 600, 600);
-    };
     draw(ctx) {
         this.animator.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 1);
     };
