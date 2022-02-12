d3da436e90ab32a62e1b351c21e7d975bd435c19
rebekah parkhurst <cparkh@uw.edu>
2022-02-11 20:44:57 -0800

    added enemy jump behavior:

--- defaultEnemies.js
+++ defaultEnemies.js
@@ -2,41 +2,54 @@ class Miniraser {
     constructor(game, x, y) {
         Object.assign(this, { game, x, y });
 
-        
         this.game = game;
         this.speed = 3;
         this.gravity = 28;
         this.falling = false;
         this.onGround = false;
+        this.jumpDistance = 100;
+        this.agro = false;
+        this.agroDistance = 400;
+        this.walkSpeed = 4;
+        this.leftJump = false;
+        this.leftJump = true;
+        this.jumpflag = false;
+
         this.velocity = { x: 0, y: 0 }
+
         // spritesheet
         this.spritesheet = ASSET_MANAGER.getAsset();
         this.animator = new Animator(ASSET_MANAGER.getAsset("./assets/characters/dino/idle_1.png"), 0, 0, 400, 400, 1, 0.12, false, true);
         this.updateBB();
-        this.agro = false;
-        this.agroDistance = 400;
-        this.walkSpeed = 4;
+        this.facing = 1; 
+
     };
 
     updateBB() {
         this.BB = new BoundingBox(this.x+200, this.y+50, 200, 200);
         
     };
+
+    
     draw(ctx) {
         this.animator.drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 1);
         ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y - this.game.camera.y, this.BB.width, this.BB.height);
     };
 
+    
     update() {
+        // console.log('FIRST: jumpflag: ' + this.jumpflag + ', onGround:' + this.onGround);
+        this.onGround = false;
 
         this.updateBB();
         const TICK = this.game.clockTick;
-        this.onGround = false;
+        const midx = (this.x + this.BB.width/2);
 
         const that = this;
         this.game.entities.forEach(function (entity) {
             //Don't collide with self, only check entity's with bounding boxes
             if (entity !== that && entity.BB && that.BB.collide(entity.BB)) {
+
                 // Currently only handling map block collisions, no entity collisions yet
                 if (entity instanceof Terrain) {
                     // Case 1: Jumping up while hitting the side
@@ -50,10 +63,103 @@ class Miniraser {
                     }
                 }
             }
+            
+            // Check to see if near a block (not quite colliding), if so, jump on it.
+            if (entity !== that && entity.BB && that.BB.inRange(entity.BB, that.jumpDistance, false)) {
+                 // only jumping if terrain is a level higher. 
+             //console.log('entity bottom: ' + entity.BB.bottom + ', enemy bottom: ' + (that.BB.bottom));
+                if (entity.BB.bottom < (that.BB.bottom)) {
+                    // console.log('top and bottom distance flagged');
+                    if (entity instanceof Terrain) {
+                        // If facing right, jump right
+                        // console.log('entity.BB.left: ' + entity.BB.left + ', that.BB.left' + that.BB.left + "true? " + (entity.BB.left > that.BB.left));
+                       if (that.facing == 1 && entity.BB.left > that.BB.left) {
+                           console.log('jumping left');
+                            that.jumpflag = true;
+                            that.leftJump = true;
+                       }
+                       // If facing left, jump left
+                       else if (that.facing == 0 && entity.BB.left < that.BB.left) {
+                           console.log('jumping right');
+                            that.jumpflag = true;
+                            that.rightJump = true;
+                       }
+                    }
+                }
+            }
         });
         //console.log('from Miniraser: Player.x' + this.game.player.x + 'Player.y' + this.game.player.y);
 
+        
+         /** BECOME AGGRO'D */
+         let {x, y} = this.game.camera.player;
+
+         if (this.BB.inRange(this.game.camera.player.BB, this.agroDistance, false)) {
+             
+             if (!this.leftJump && !this.rightJump) {
+                 // player is on the left
+                if (x < midx) {
+                    this.x -= this.walkSpeed;
+                    this.facing = 1;
+                }
+                // player is on the right
+                else if (x > midx) {
+                    this.x += this.walkSpeed;
+                    this.facing = 0;
+                }
+                else {
+                    this.velocity.x = 0;
+                }
+            }
+         }
+         else {
+             this.velocity.x = 0;
+         }
+
         /** FALLING */
+        // console.log('ground: ' + this.onGround + ', left jump: ' + this.leftJump + ', right jump: ' + this.rightJump);
+        if (!this.onGround && !this.leftJump && !this.rightJump) {
+            this.falling = true;
+            console.log('falling is true');
+        }
+        else {
+            this.falling = false;
+        }
+        if (this.falling &! this.onGround) {
+            
+        }
+
+
+        /** JUMPING **/
+        // Stops the jump once player hits the ground.
+        if (this.onGround && !this.jumpflag) {
+            this.falling = false;
+            this.velocity.y = 0;
+            this.rightJump = false;
+            this.leftJump = false;
+        }
+        // console.log('SECOND: jumpflag: ' + this.jumpflag + ', onGround:' + this.onGround);
+        if (this.jumpflag && this.onGround) {
+            this.velocity.y = -1000;
+            this.jumpflag = false;
+        }
+
+        if (this.rightJump) {
+            this.onGround = false;
+            this.velocity.x = 6;
+            this.x += this.velocity.x;
+        }
+        else if (this.leftJump) {
+            this.onGround = false;
+            this.velocity.x = 6;
+            this.x -= this.velocity.x;
+        }
+
+        // // If not on ground, velocity should be increasing
+        // if(!this.onGround || this.falling) {
+        //     this.velocity.y += this.gravity;
+        // }
+
         if (!this.onGround) {
             this.falling = true;
         }
@@ -61,33 +167,15 @@ class Miniraser {
             this.falling = false;
             this.velocity.y = 0;
         }
-        if (this.falling && !this.onGround) {
+        if (this.falling) {
             this.velocity.y += this.gravity;
         }
 
-        /** BECOME AGGRO'D */
-        let {x, y} = this.game.camera.player;
+        //Update falling status
+        // if(this.velocity.y > 0 && !this.leftJump && !this.rightJump) this.falling = true;
 
-        console.log("enemy x=" + this.x + ", player x=" + x);
 
-        if (Math.abs(x - (this.x+100)) < this.agroDistance || Math.abs((this.x+100) - x) < this.agroDistance) {
-            // player is on the left
-            if (x < this.x) {
-                this.x -= this.walkSpeed;
-            }
-            // player is on the right
-            else if (x > this.x) {
-                this.x += this.walkSpeed;
-            }
-            else {
-                this.velocity.x = 0;
-            }
-        }
-        else {
-            this.velocity.x = 0;
-        }
 
-        
         // Don't walk through blocks.
         if (this.side) {
             this.velocity.x = 0;
