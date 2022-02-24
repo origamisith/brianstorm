b4d6b8434e86ac1850f129b8223fc1a364ba8e92
rinpham217 <rinpham1011@gmail.com>
2022-02-24 01:26:03 -0800

    updated water level

--- levels/levelWater.js
+++ levels/levelWater.js
@@ -12,19 +12,16 @@ class levelWaterGenerator {
   } 
   static singleSeahorseGenerate(count) {
     const singleSeahorse = [];
-    let place = 0;
     for (let i = 0; i < count; i++) {
-      //place = Math.floor(Math.random());
-      singleSeahorse[i] = {x: 100 + place, y: 50}
-      place += 700;
+      singleSeahorse[i] = {x: 500, y: 50}
     }
     return singleSeahorse;
   }
+   
   static fishes(count) {
     const fish = [];
     let place = 0;
     for (let i = 0; i < count; i++) {
-      //place = Math.floor(Math.random());
       fish[i] = {x: 1000 + place, y: 50}
       place += 700;
     }
@@ -33,11 +30,8 @@ class levelWaterGenerator {
 
   static sharkGenerate(count) {
     const shark = [];
-    let place = 0;
     for (let i = 0; i < count; i++) {
-      //place = Math.floor(Math.random());
-      shark[i] = {x: 600 + place, y: 100 + Math.round(50)}
-      place += 500;
+      shark[i] = {x: 1250, y: 100 }
     }
     return shark;
   }
@@ -48,28 +42,23 @@ class levelWaterGenerator {
     for (let i = 0; i < count; i++) {
       let place_y = Math.floor(Math.random());
       squid[i] = {x: 500 + place, y: 50 + place_y}
-      place += 700 + Math.random();
+      place += 700 ;
     }
     return squid;
   }
 
   static squid_InkGenerate(count) {
     const squid_ink = [];
-    let place = 0;
     for (let i = 0; i < count; i++) {
-      place = Math.floor(Math.random());
-      squid_ink[i] = {x: 1200 + place, y: 50}
-      place += 600;
+      squid_ink[i] = {x: 1850, y: 50}
     }
     return squid_ink;
   }
 
   static starfishGenerate(count) {
     const starfish = [];
-    let place = 0;
     for (let i = 0; i < count; i++) {
-      place = Math.floor(Math.random());
-      starfish[i] = {x: 650 + place, y: 50}
+      starfish[i] = {x: 0, y: 50}
     }
     return starfish;
   }
@@ -79,10 +68,10 @@ class levelWaterGenerator {
 
 const levelWater = {
   starfish: levelWaterGenerator.starfishGenerate(5),
-  squid: levelWaterGenerator.squidGenerate(7),
+  squid: levelWaterGenerator.squidGenerate(5),
   squid_ink: levelWaterGenerator.squid_InkGenerate(5),
   shark: levelWaterGenerator.sharkGenerate(1),
-  fish: levelWaterGenerator.fishes(5),
+  fish: levelWaterGenerator.fishes(20),
   doubleSeahorses: levelWaterGenerator.doubleseahorsesGenerate(2),
   singleSeahorse: levelWaterGenerator.singleSeahorseGenerate(5),
   music: "./assets/music/FreedomM.mp3",
