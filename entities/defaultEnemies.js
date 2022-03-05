71ff13dad64affd49c3d03855e979f55a1dd036c
rebekah parkhurst <cparkh@uw.edu>
2022-03-04 22:10:39 -0800

    rebekah2 works

--- entities/defaultEnemies.js
+++ entities/defaultEnemies.js
@@ -8,14 +8,18 @@ class Miniraser {
         this.falling = false;
         this.onGround = false;
         this.jumpDistance = 20;
-        this.agroDistance = 800;
-        this.walkSpeed = 4;
+        this.agro = false;
+        this.agroDistance = 600;
+        this.walkSpeed = 12;
         this.leftJump = false;
         this.leftJump = true;
         this.jumpflag = false;
+        this.jumpCooldown = 0;
         this.elapsedTime = 0;
         this.hp = 50;
         this.stunned = false;
+        this.bump = false;
+
 
         this.state = 0;
         this.facing = 1; // 0=right, 1=left
@@ -53,16 +57,19 @@ class Miniraser {
     };
 
     updateBB() {
-
+        this.lastBB = this.BB;
         this.BB = new BoundingBox(this.x, this.y, 200*this.scale, 360*this.scale)
     };
 
     update() {
+        
 
         this.updateAnimations();
         this.onGround = false;
+        this.side = false;
 
         this.updateBB();
+        
         const TICK = this.game.clockTick;
         const midx = (this.x + this.BB.width/2);
         this.elapsedTime += TICK;
@@ -71,83 +78,133 @@ class Miniraser {
             this.stunTimer -= 10*TICK;
             if (this.stunTimer <= 0) {this.stunned = false;}
         }
-
+        if (this.jumpCooldown > 0) {
+            this.jumpCooldown -= 10*TICK;
+        }
         //if not stunned
         else {this.state = 0;}
 
+        if (this.bump) {
+            // console.log("bumping");
+            this.velocity.y = 0;
+            this.bump = false;
+        }
+
+        
+
         const that = this;
+        let collided = [];
+        let jumpBlocks = [];
         this.game.entities.forEach(function (entity) {
             //Don't collide with self, only check entity's with bounding boxes
             if (entity !== that && entity.BB && that.BB.collide(entity.BB)) {
-
-                // Currently only handling map block collisions, no entity collisions yet
                 if (entity instanceof Terrain) {
-                    // Case 1: Jumping up while hitting the side
-                    // Case 2: Walking into the side while on the ground
-                    if((!that.onGround && that.velocity.y < 0) || (that.BB.bottom >= entity.BB.bottom)) {
-                        that.side = true;
-                    }
-                    // Case 3: Falling onto flat ground
-                    else {
-                        that.onGround = true;
-                        that.y = entity.BB.top - that.BB.height;
-                    }
+                    collided.push(entity);  
                 }
                 else if (entity instanceof Scribble) {
                     that.state = 1;
-
                     that.stunned = true;
                     that.stunTimer = 10;
-
                 }
+                else if (entity instanceof Player) {
+                    if (that.facing == 0) {
+                        that.facing = 1; 
+                        that.velocity.x *= -1;
+                    } else if (that.facing == 1) {
+                        that.facing = 0;
+                        that.velocity.x *= -1;
+                    }
+                }
+                
             }
-
-            // Check to see if near a block (not quite colliding), if so, jump on it.
-            if (entity !== that && entity.BB && that.BB.inRange(entity.BB, that.jumpDistance, false)) {
-                // only jumping if terrain is a level higher.
-                //console.log('entity bottom: ' + entity.BB.bottom + ', enemy bottom: ' + (that.BB.bottom));
-                if (entity.BB.bottom < that.BB.bottom && Math.abs(entity.BB.bottom - (that.BB.bottom)) > params.blockSize) {
-                    if (entity instanceof Terrain) {
-                        // If facing right, jump right
-                        // console.log('entity.BB.left: ' + entity.BB.left + ', that.BB.left' + that.BB.left + "true? " + (entity.BB.left > that.BB.left));
-                       if (that.facing === 1 && entity.BB.left > that.BB.left) {
-                           that.facing = 1;
-                            that.jumpflag = true;
-                            that.leftJump = true;
-                       }
-                       // If facing left, jump left
-                       else if (that.facing === 0 && entity.BB.left < that.BB.left) {
-                           that.facing = 0;
-                            that.jumpflag = true;
-                            that.rightJump = true;
-                        }
-                    } else if (entity instanceof Player) {
-                        if (entity.BB.topCollide(that.BB) && that.elapsedTime > 0.8) {
-                            that.hp -= 5;
-                            // console.log("miniraser HP: " + that.hp);
-                            that.elapsedTime = 0;
-                        }
+            else if (entity !== that && entity.BB && that.BB.inRange(entity.BB, that.jumpDistance, false)) {
+                if (entity instanceof Terrain) {
+                    if (entity.BB.bottom < params.blockSize + that.BB.bottom && (that.BB.bottom > 400) && (entity.BB.bottom < 800)) {
+                        jumpBlocks.push(entity);
                     }
                 }
             }
         });
 
+        
+        collided.sort((boundary1, boundary2) => distance({ x: this.BB.cx, y: this.BB.cy }, 
+            { x: boundary1.BB.cx, y: boundary1.BB.cy }) - 
+            distance({ x: this.BB.cx, y: this.BB.cy }, { x: boundary2.BB.cx, y: boundary2.BB.cy }));
+        
+        // jumpBlocks.sort((boundary1, boundary2) => distance({ x: this.BB.cx, y: this.BB.cy }, 
+        //     { x: boundary1.BB.cx, y: boundary1.BB.cy }) - 
+        //     distance({ x: this.BB.cx, y: this.BB.cy }, { x: boundary2.BB.cx, y: boundary2.BB.cy }));
+
+        collided.forEach(function (entity) {
+            if (entity !== that && entity.BB && that.BB.collide(entity.BB)) {
+                if (entity instanceof Terrain) {
+                    // console.log(entity.BB.bottom);
+                    // Case 1: Jumping up while hitting the side
+                    // Case 2: Walking into the side while on the ground
+                    // Case 3: Bumping the ceiling
+                    if((!that.onGround && that.velocity.y < 0) || (that.BB.bottom >= entity.BB.bottom)) {
+                        if (Math.abs(that.BB.top - entity.BB.bottom) < 100) {
+                            console.log("case 3");
+                        that.bump = true;
+                        that.velocity.y = that.velocity.y * -0.5;
+                        } else {
+                            that.side = true;
+                            if (!that.onGround) that.falling = true;
+                        }
+                        
+                    }
+                    // Case 4: Falling onto flat ground
+                    else {
+                        // console.log("case 4");
+                        that.onGround = true;
+                        that.y = entity.BB.top - that.BB.height;
+                    }
+                }
+            }
+            
+        });
+
+        /** JUMP WHEN COLLIDING WITH A BLOCK */
+        jumpBlocks.forEach(function (entity) {
+            if (that.onGround && that.jumpCooldown <= 0) {
+                that.jumpCooldown = 20;
+                if (that.facing === 0) {
+                    that.jumpflag = true;
+                    that.rightJump = true;
+                }
+                else if (that.facing === 1) {
+                    that.jumpflag = true;
+                    that.leftJump = true;
+                }
+            }
+        });
+
+
+
+        /** DON'T GET STUCK ON THE CEILING OR JUMP INTO IT! */
+        if (this.BB.bottom < 500) {
+            this.side = false;
+            this.jumpflag = false;
+        }
+
+        /** DIE */
         if (this.hp === 0) {
             this.removeFromWorld = true;
         }
+
         /** BECOME AGGRO'D */
         let {x, y} = this.game.camera.player;
 
-        if (this.BB.inRange(this.game.camera.player.BB, this.agroDistance, false) &! this.stunned) {
+        if (this.BB.inRange(this.game.camera.player.BB, this.agroDistance, false) &! this.stunned && this.onGround) {
 
-            if (!this.leftJump && !this.rightJump) {
+            if (!this.leftJump && !this.rightJump && !this.stunned && !this.side) {
                 // player is on the left
-                if (x < midx) {
+                if (x < midx && Math.abs(x-midx < 10) && !(this.side && this.facing === 1)) {
                     this.x -= this.walkSpeed;
                     this.facing = 1;
                 }
                 // player is on the right
-                else if (x > midx) {
+                else if (x > midx && Math.abs(x-midx > 10) && !(this.side && this.facing === 0)) {
                     this.x += this.walkSpeed;
                     this.facing = 0;
                 }
@@ -164,7 +221,7 @@ class Miniraser {
         // console.log('ground: ' + this.onGround + ', left jump: ' + this.leftJump + ', right jump: ' + this.rightJump);
         this.falling = !this.onGround && !this.leftJump && !this.rightJump;
         if (this.falling &&! this.onGround) {
-
+            this.velocity.y += this.gravity;
         }
 
 
@@ -210,14 +267,7 @@ class Miniraser {
         }
 
         //Update falling status
-        // if(this.velocity.y > 0 && !this.leftJump && !this.rightJump) this.falling = true;
-
-
-
-        // Don't walk through blocks.
-        if (this.side) {
-            this.velocity.x = 0;
-        }
+        // if(this.velocity.y > 0 && !this.leftJump && !this.rightJump) this.falling = true;        
 
 
         /** UNIVERSAL POSITION UPDATE **/
